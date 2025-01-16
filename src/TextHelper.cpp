656f41412bbeb79590631e529c41c87b4ca2dc4b
KSS <kssbrawl@gmail.com>
2025-01-15 18:07:44 -0600

    Implement and match TextHelper::TryAllocateBuffer

--- src/TextHelper.cpp
+++ src/TextHelper.cpp
@@ -60,5 +60,67 @@ bool TextHelper::AllocateBufferWithFallback(i32 width, i32 height, D3DFORMAT for
     }
     return false;
 }
+
+struct THBITMAPINFO
+{
+    BITMAPINFOHEADER bmiHeader;
+    RGBQUAD bmiColors[17];
+};
+
+#pragma function(memset)
+#pragma var_order(imageWidthInBytes, deviceContext, originalBitmapObj, padding, bitmapInfo, formatInfo, bitmapObj,     \
+                  bitmapData)
+bool TextHelper::TryAllocateBuffer(i32 width, i32 height, D3DFORMAT format)
+{
+    HGDIOBJ originalBitmapObj;
+    u8 *bitmapData;
+    HBITMAP bitmapObj;
+    FormatInfo *formatInfo;
+    THBITMAPINFO bitmapInfo;
+    u32 padding;
+    HDC deviceContext;
+    i32 imageWidthInBytes;
+
+    ReleaseBuffer();
+    memset(&bitmapInfo, 0, sizeof(THBITMAPINFO));
+    formatInfo = GetFormatInfo(format);
+    if (formatInfo == NULL)
+    {
+        return false;
+    }
+    imageWidthInBytes = ((((width * formatInfo->bitCount) / 8) + 3) / 4) * 4;
+    bitmapInfo.bmiHeader.biSize = sizeof(THBITMAPINFO);
+    bitmapInfo.bmiHeader.biWidth = width;
+    bitmapInfo.bmiHeader.biHeight = -(height + 1);
+    bitmapInfo.bmiHeader.biPlanes = 1;
+    bitmapInfo.bmiHeader.biBitCount = formatInfo->bitCount;
+    bitmapInfo.bmiHeader.biSizeImage = height * imageWidthInBytes;
+    if (format != D3DFMT_X1R5G5B5 && format != D3DFMT_X8R8G8B8)
+    {
+        bitmapInfo.bmiHeader.biCompression = 3;
+        ((u32 *)bitmapInfo.bmiColors)[0] = formatInfo->redMask;
+        ((u32 *)bitmapInfo.bmiColors)[1] = formatInfo->greenMask;
+        ((u32 *)bitmapInfo.bmiColors)[2] = formatInfo->blueMask;
+        ((u32 *)bitmapInfo.bmiColors)[3] = formatInfo->alphaMask;
+    }
+    bitmapObj = CreateDIBSection(NULL, (BITMAPINFO *)&bitmapInfo, 0, (void **)&bitmapData, NULL, 0);
+    if (bitmapObj == NULL)
+    {
+        return false;
+    }
+    memset(bitmapData, 0, bitmapInfo.bmiHeader.biSizeImage);
+    deviceContext = CreateCompatibleDC(NULL);
+    originalBitmapObj = SelectObject(deviceContext, bitmapObj);
+    m_hdc = deviceContext;
+    m_gdiObj2 = bitmapObj;
+    m_buffer = bitmapData;
+    m_imageSizeInBytes = bitmapInfo.bmiHeader.biSizeImage;
+    m_gdiObj = originalBitmapObj;
+    m_width = width;
+    m_height = height;
+    m_format = format;
+    m_imageWidthInBytes = imageWidthInBytes;
+    return true;
+}
 #pragma optimize("", on)
 }; // namespace th08
