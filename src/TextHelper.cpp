238b62ac2d6109959348136cbcac1e688811674b
KSS <kssbrawl@gmail.com>
2025-01-15 18:03:32 -0600

    Implement and match TextHelper::ReleaseBuffer

--- src/TextHelper.cpp
+++ src/TextHelper.cpp
@@ -20,5 +20,27 @@ TextHelper::~TextHelper()
 {
     ReleaseBuffer();
 }
+
+bool TextHelper::ReleaseBuffer()
+{
+    if (m_hdc)
+    {
+        SelectObject(m_hdc, m_gdiObj);
+        DeleteDC(m_hdc);
+        DeleteObject(m_gdiObj2);
+        m_format = (D3DFORMAT)-1;
+        m_width = 0;
+        m_height = 0;
+        m_hdc = 0;
+        m_gdiObj2 = 0;
+        m_gdiObj = 0;
+        m_buffer = NULL;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 #pragma optimize("", on)
 }; // namespace th08
