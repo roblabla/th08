ee233befc00483530a99c2ca81131fb88c55524c
KSS <kssbrawl@gmail.com>
2025-01-15 18:04:46 -0600

    Implement and match TextHelper::AllocateBufferWithFallback

--- src/TextHelper.cpp
+++ src/TextHelper.cpp
@@ -42,5 +42,23 @@ bool TextHelper::ReleaseBuffer()
         return false;
     }
 }
+
+bool TextHelper::AllocateBufferWithFallback(i32 width, i32 height, D3DFORMAT format)
+{
+    if (TryAllocateBuffer(width, height, format))
+    {
+        return true;
+    }
+
+    if (format == D3DFMT_A1R5G5B5 || format == D3DFMT_A4R4G4B4)
+    {
+        return TryAllocateBuffer(width, height, D3DFMT_A8R8G8B8);
+    }
+    if (format == D3DFMT_R5G6B5)
+    {
+        return TryAllocateBuffer(width, height, D3DFMT_X8R8G8B8);
+    }
+    return false;
+}
 #pragma optimize("", on)
 }; // namespace th08
