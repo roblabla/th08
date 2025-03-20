833ed4d06d281270ca4945611563c358463dfad0
KSS <kssbrawl@gmail.com>
2025-03-19 22:16:28 -0500

    Add method declarations for SoundPlayer, rename unk620, add preliminary SoundIdx enum

--- src/SoundPlayer.hpp
+++ src/SoundPlayer.hpp
@@ -9,6 +9,10 @@
 
 namespace th08
 {
+enum SoundIdx
+{
+};
+
 struct SoundPlayerCommand
 {
     i32 arg1;
@@ -20,6 +24,33 @@ struct SoundPlayerCommand
 class SoundPlayer
 {
   public:
+    SoundPlayer();
+
+    ZunResult InitializeDSound(HWND window);
+    ZunResult InitSoundBuffers();
+    ZunResult Release();
+
+    ZunResult LoadSound(i32 idx, char *path);
+    static WAVEFORMATEX *GetWavFormatData(u8 *soundData, char *formatString, i32 *formatSize,
+                                          u32 fileSizeExcludingFormat);
+
+    void QueueCommand(i32 opcode, i32 arg1, i32 arg2, char *unused);
+    i32 ProcessQueues();
+    void PlaySoundByIdx(SoundIdx idx, i32 unused);
+    void PlaySoundPositionedByIdx(SoundIdx idx, f32 pan);
+    ZunResult StartBGM(char *path);
+    ZunResult ReopenBGM(char *path);
+    ZunResult PreloadBGM(char *path);
+    ZunResult LoadBGM(char *path);
+    void FreePreloadedBGM();
+    void StopBGM();
+    void FadeOut(f32 seconds);
+
+    static DWORD WINAPI BGMPlayerThread(LPVOID lpThreadParameter);
+
+    i32 GetFmtIndexByName(char *name);
+    ZunResult LoadFmt(char *path);
+
     LPDIRECTSOUND dsoundHdl;
     i32 unk4;
     LPDIRECTSOUNDBUFFER soundBuffers[128];
@@ -31,7 +62,7 @@ class SoundPlayer
     DWORD bgmThreadId;
     HANDLE bgmThreadHandle;
     i32 unk61c;
-    u32 unk620[12];
+    i32 soundBuffersToPlay[12];
     u32 unk650[12];
     u32 unk680[12][128];
     ThBgmFormat *unk1e80[16];
